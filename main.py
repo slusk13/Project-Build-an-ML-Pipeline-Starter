d06ec0d271c6a367ba92b782bc41836c0c0a3edb
Ryan Lusk <stevenlusk22@gmail.com>
2025-03-31 15:41:08 -0500

    Uncommented section from _steps. Now the pipeline will run if steps weren't specified.

--- main.py
+++ main.py
@@ -13,10 +13,7 @@ _steps = [
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-#    "test_regression_model"
+    "test_regression_model"
 ]
 
 
