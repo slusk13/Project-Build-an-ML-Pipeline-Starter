561e8cafd0c63fc88274c2f4fb2352345c2c60a4
Ryan Lusk <stevenlusk22@gmail.com>
2025-03-31 15:58:17 -0500

    Added comment and valid geolocation filter to basic_cleaning/run.py.

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,7 +29,7 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
-
+    # Filter out invalid latitude/longitude ranges
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
     # Save the cleaned file
