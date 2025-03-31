59c82c05cce74e77d2df24f355e8957210058910
Ryan Lusk <stevenlusk22@gmail.com>
2025-03-30 21:22:38 -0500

    Added src repository link in config.yaml. Implemented basic_cleaning pipeline in main.py.

--- main.py
+++ main.py
@@ -51,10 +51,20 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['src_repository']}/basic_cleaning",
+                "main",
+                version='main',
+                env_manager="conda",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_data",
+                    "output_description": "Data after basic cleaning",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
